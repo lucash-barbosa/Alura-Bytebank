f828d5553ce0ee23bbd846ae8b0f085e6ddbdf5b
Lucaslkj <lucas565lkj@gmail.com>
2022-06-02 14:26:59 -0300

    adicionando valor à transferencia

--- index.js
+++ index.js
@@ -7,9 +7,11 @@ const cliente2 = new Cliente("Alice", 88822233309);
 
 const contaCorrenteRicardo = new ContaCorrente(1001, cliente1);
 contaCorrenteRicardo.depositar(500);
+contaCorrenteRicardo.sacar(100);
 
 const conta2 = new ContaCorrente(102, cliente2);
 
-contaCorrenteRicardo.transferir(200, conta2);
+let valor = 200;
+contaCorrenteRicardo.transferir(valor, conta2);
 
 console.log(ContaCorrente.numeroDeContas);
